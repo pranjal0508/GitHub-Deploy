96963a135b01f6d3bada1607e3d158d473f09396
Pranjal Gupta <pranjalgg00@gmail.com>
2024-03-18 18:58:56 +0530

    feature : React Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home.jsx';
+import Contact from './pages/Contact.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/GitHub-Deploy/",
+    element: <App />,
+    children: [
+      {
+        path: "/GitHub-Deploy/",
+        element: <Home />
+      },
+      {
+        path: "/GitHub-Deploy/contact",
+        element: <Contact />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
